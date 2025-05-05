4a9486c9891a1000551e060e301ea365754b04b4
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-05 16:01:38 +0300

    Add Employee model

---
+++ RestaurantReservation.Db/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RestaurantReservation.Db.Models;
+
+public class Employee
+{
+    public int EmployeeId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Position { get; set; }
+    public int RestaurantId { get; set; }
+    public Restaurant Restaurant { get; set; }
+    public List<Order> Orders { get; set; } = new List<Order>();
+}
