8431fb44104557271451aa5c9a62a5fde71079ed
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-05 13:54:45 +0300

    Add RestaurantReservationDbContext

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantReservation.Db;
+
+public class RestaurantReservationDbContext : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+}
