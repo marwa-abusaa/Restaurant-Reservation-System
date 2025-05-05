b9c26e2468828c16b0312bb9fabee112497d8653
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-05 18:28:49 +0300

    Add DbContext configuration and entity relationships with cascade restrictions

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,16 +1,66 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RestaurantReservation.Db.Models;
 
 namespace RestaurantReservation.Db;
 
 public class RestaurantReservationDbContext : DbContext
 {
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<MenuItem> MenuItems { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Table> Tables { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.Table)
+            .WithMany()
+            .HasForeignKey(r => r.TableId)
+            .OnDelete(DeleteBehavior.Restrict); 
+
+        modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.Customer)
+            .WithMany()
+            .HasForeignKey(r => r.CustomerId)
+            .OnDelete(DeleteBehavior.Cascade); 
+
+        modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.Restaurant)
+            .WithMany()
+            .HasForeignKey(r => r.RestaurantId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Reservation)
+            .WithMany()
+            .HasForeignKey(o => o.ReservationId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Employee)
+            .WithMany()
+            .HasForeignKey(o => o.EmployeeId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(oi => oi.Order)
+            .WithMany()
+            .HasForeignKey(oi => oi.OrderId)
+            .OnDelete(DeleteBehavior.Restrict);
 
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        var config = new ConfigurationBuilder().AddJsonFile("C:\\Users\\hp\\source\\repos\\RestaurantReservation\\RestaurantReservation\\AppSettings.json").Build();
 
+        string connectionString = config.GetConnectionString("RestaurantReservationCore")!;
+
+        optionsBuilder.UseSqlServer(connectionString);
     }
 }
