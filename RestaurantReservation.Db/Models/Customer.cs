a94fa0f9760b5eadf2c66bf25fc54a26fc2bad6f
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-05 16:00:09 +0300

    Add Customer model

---
+++ RestaurantReservation.Db/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿
+namespace RestaurantReservation.Db.Models;
+
+public class Customer
+{
+    public int CustomerId { set; get; }
+    public string FirstName { set; get; }
+    public string LastName { set; get; }
+    public string Email { set; get; }
+    public string PhoneNumber { set; get; }
+    public List<Reservation> Reservations { set; get; } = new List<Reservation>();
+}
