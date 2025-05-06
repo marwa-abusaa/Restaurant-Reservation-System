87df95a55b450d710be0ee3c8c6ca5166a494f0c
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-06 18:37:08 +0300

    add MenuItemRepository & implement ListOrderedMenuItems method

---
+++ RestaurantReservation.Db/Repositories/MenuItemRepository.cs
@@ -0,0 +1,25 @@
+﻿using RestaurantReservation.Db.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RestaurantReservation.Db.Repositories;
+
+public class MenuItemRepository : Repository<Employee>
+{
+    private RestaurantReservationDbContext _context;
+
+    public MenuItemRepository(RestaurantReservationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<MenuItem>> ListOrderedMenuItems (int reservationId)
+    {
+        return await _context.OrderItems
+                    .Where(oi => oi.Order.ReservationId == reservationId)
+                    .Include(oi => oi.MenuItem)
+                    .Select(oi => oi.MenuItem)
+                    .Distinct()
+                    .ToListAsync();
+    }
+}
