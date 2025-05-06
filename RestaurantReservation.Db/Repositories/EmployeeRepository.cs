ab0007ca3b81b22fcefed49da0480f8aa95ce373
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-06 17:39:46 +0300

    add EmployeeRepository & implement ListManagers method in it

---
+++ RestaurantReservation.Db/Repositories/EmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
+using RestaurantReservation.Db.Models.Enum;
+
+namespace RestaurantReservation.Db.Repositories;
+
+public class EmployeeRepository : Repository<Employee>
+{
+    private RestaurantReservationDbContext _context;
+
+    public EmployeeRepository (RestaurantReservationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Employee>> ListManagers()
+    {
+        return await _context.Employees
+                    .Where(e => e.Position == EmployeePosition.Manager)
+                    .ToListAsync();
+    }
+}
