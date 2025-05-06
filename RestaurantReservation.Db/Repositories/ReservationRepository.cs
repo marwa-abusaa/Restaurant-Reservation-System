6c33dc0178aa174c803b28303764eefb391b848a
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-06 17:52:52 +0300

    add ReservationRepository & implement GetReservationsByCustomer in it

---
+++ RestaurantReservation.Db/Repositories/ReservationRepository.cs
@@ -0,0 +1,22 @@
+﻿using RestaurantReservation.Db.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RestaurantReservation.Db.Repositories;
+
+public class ReservationRepository : Repository<Employee>
+{
+    private RestaurantReservationDbContext _context;
+
+    public ReservationRepository(RestaurantReservationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Reservation>> GetReservationsByCustomer(int customerId)
+    {
+        return await _context.Reservations
+                    .Where(r => r.CustomerId == customerId)
+                    .ToListAsync();
+    }
+}
