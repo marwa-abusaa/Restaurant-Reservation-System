1f667accc22e1d9f70f650e4443ae2f9947658a6
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-06 15:02:26 +0300

    change name of ItemId property to MenuItemId

--- RestaurantReservation.Db/Models/OrderItem.cs
+++ RestaurantReservation.Db/Models/OrderItem.cs
@@ -7,6 +7,6 @@ public class OrderItem
     public int Quantity { get; set; }
     public int OrderId { get; set; }
     public Order Order { get; set; }
-    public int ItemId { get; set; }
+    public int MenuItemId { get; set; }
     public MenuItem MenuItem { get; set; }
 }
