fa0abbabb4b7ca38fe0e13cbbf500f52c4757951
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-06 16:34:26 +0300

    add IRepository interface for create,update,delete methods

---
+++ RestaurantReservation.Db/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RestaurantReservation.Db.Interfaces;
+
+public interface IRepository<TEntity> where TEntity: class
+{
+    public Task Create(TEntity model);
+    public Task Update(TEntity model);
+    public Task DeleteById(int id);
+    public Task<TEntity> GetById(int id);
+}
