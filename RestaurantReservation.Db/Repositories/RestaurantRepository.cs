ac8aa135600392ed268571bae46aaf300ad076d9
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-06 23:56:53 +0300

    add RevenueResult class & implement CalculateRestaurantRevenue method in RestaurantRepository

---
+++ RestaurantReservation.Db/Repositories/RestaurantRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db.Repositories;
+
+public class RestaurantRepository : Repository<Employee>
+{
+    private RestaurantReservationDbContext _context;
+
+    public RestaurantRepository(RestaurantReservationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<decimal> CalculateRestaurantRevenue(int restaurantId)
+    {
+
+        var revenue = await _context
+        .Set<RevenueResult>()
+        .FromSqlRaw($"SELECT dbo.GetTotalRevenueByRestaurant({0}) AS TotalRevenue", restaurantId)
+        .FirstOrDefaultAsync();
+
+        return revenue.TotalRevenue;
+    }
+}
