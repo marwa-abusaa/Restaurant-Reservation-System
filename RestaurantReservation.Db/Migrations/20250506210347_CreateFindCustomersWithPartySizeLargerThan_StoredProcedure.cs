b035780819512f4eafec5fcb399c14518ff7ffe8
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-07 00:11:53 +0300

    Add FindCustomersWithPartySizeLargerThan procedure via migration

---
+++ RestaurantReservation.Db/Migrations/20250506210347_CreateFindCustomersWithPartySizeLargerThan_StoredProcedure.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateFindCustomersWithPartySizeLargerThan_StoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                CREATE PROCEDURE sp_FindCustomersWithPartySizeLargerThan (
+                	@MinPartySize INT
+                )
+                AS BEGIN 
+                	SELECT DISTINCT c.*
+                    FROM Customers c
+                    JOIN Reservations r ON c.CustomerId = r.CustomerId
+                    WHERE r.PartySize > @MinPartySize;
+                END;                  
+                """
+                );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE sp_FindCustomersWithPartySizeLargerThan");
+        }
+    }
+}
