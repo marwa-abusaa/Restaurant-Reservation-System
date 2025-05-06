fc64c91ecede576af2ac0905bb3115c59972edb8
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-07 00:16:10 +0300

    Add CustomerRepository & implement GetCustomersWithReservationsAbovePartySize method in it

---
+++ RestaurantReservation.Db/Repositories/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models.Enum;
+using RestaurantReservation.Db.Models.Views;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db.Repositories;
+
+public class CustomerRepository : Repository<Employee>
+{
+    private RestaurantReservationDbContext _context;
+
+    public CustomerRepository(RestaurantReservationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Customer>> GetCustomersWithReservationsAbovePartySize(int partySize)
+    {
+        return await _context.Customers.FromSql($"EXEC sp_FindCustomersWithPartySizeLargerThan {partySize}").ToListAsync();
+    }
+}
