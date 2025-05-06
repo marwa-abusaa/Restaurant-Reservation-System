ce063aa4176b9da444a57dadfb0a73e1043dfabe
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-06 18:12:22 +0300

    add OrderRepository & implement ListOrdersAndMenuItems in it

---
+++ RestaurantReservation.Db/Repositories/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using RestaurantReservation.Db.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RestaurantReservation.Db.Repositories;
+
+public class OrderRepository : Repository<Employee>
+{
+    private RestaurantReservationDbContext _context;
+
+    public OrderRepository(RestaurantReservationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Order>> ListOrdersAndMenuItems(int reservationId)
+    {
+        return await _context.Orders
+                    .Where(o => o.ReservationId == reservationId)
+                    .Include(o => o.OrderItems)
+                        .ThenInclude(mi => mi.MenuItem)
+                    .ToListAsync();
+    }
+}
