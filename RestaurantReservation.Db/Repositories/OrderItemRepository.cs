40b3079f201e6aba9680fcf69ebad33aff80dc9c
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-07 17:06:30 +0300

    add OrderItem & Table Repository classes

---
+++ RestaurantReservation.Db/Repositories/OrderItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db.Repositories;
+
+class OrderItemRepository : Repository<Employee>
+{
+    private RestaurantReservationDbContext _context;
+
+    public OrderItemRepository(RestaurantReservationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+}
---
+++ RestaurantReservation.Db/Repositories/TableRepository.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db.Repositories
+{
+    class TableRepository : Repository<Employee>
+    {
+        private RestaurantReservationDbContext _context;
+
+        public TableRepository(RestaurantReservationDbContext context) : base(context)
+        {
+            _context = context;
+        }    
+    }
+}
