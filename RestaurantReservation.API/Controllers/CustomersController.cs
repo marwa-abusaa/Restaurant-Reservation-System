14cc8672bc3ff847e71d30557f077d1a4a6b4943
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-28 12:44:44 +0300

    add CustomersController & implement GetCustomer method

---
+++ RestaurantReservation.API/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.Customers;
+using RestaurantReservation.Db.Repositories;
+
+namespace RestaurantReservation.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController : ControllerBase
+{
+    private CustomerRepository _customerRepository;
+    private readonly IMapper _mapper;
+
+    public CustomersController(IMapper mapper, CustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet("{id}")]   
+    public async Task<ActionResult<CustomerDto>> GetCustomer(int id)
+    {
+        var customer = await _customerRepository.GetById(id);
+        if (customer == null)
+            return NotFound();
+
+        return Ok(_mapper.Map<CustomerDto>(customer));
+    }
+
+    
+
+}
