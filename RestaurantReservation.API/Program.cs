39d92d9a76bc1b74cfcc8e560092c5f2c6314e8d
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-28 13:01:10 +0300

    update Program.cs for testing and configuration

--- RestaurantReservation.API/Program.cs
+++ RestaurantReservation.API/Program.cs
@@ -1,18 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db;
+using RestaurantReservation.Db.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<RestaurantReservationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RestaurantReservationCore")));
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<CustomerRepository>();
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+//builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
 
 app.UseHttpsRedirection();
 
