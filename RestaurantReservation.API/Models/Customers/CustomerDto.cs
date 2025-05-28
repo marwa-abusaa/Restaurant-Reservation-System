a1cfc04b298b62d5f3ae3e614616e4ae52eff9cf
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-28 12:40:05 +0300

    add Customers model for related DTOs (CustomerDto, CustomerCreateDto, CustomerUpdateDto)

---
+++ RestaurantReservation.API/Models/Customers/CustomerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantReservation.API.Models.Customers;
+
+public class CustomerDto
+{
+    public int CustomerId { set; get; }
+    public string? FirstName { set; get; }
+    public string? LastName { set; get; }
+    public string? PhoneNumber { set; get; }
+}
