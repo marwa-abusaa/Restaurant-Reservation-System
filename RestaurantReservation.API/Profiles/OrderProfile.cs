29a6833f2dca1e3734815d610941016c3541d317
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-28 15:07:26 +0300

    add OrderProfile for AutoMapper configuration

---
+++ RestaurantReservation.API/Profiles/OrderProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.Orders;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.API.Profiles;
+
+public class OrderProfile : Profile
+{
+    public OrderProfile()
+    {
+        CreateMap<Order, OrderDto>();
+        CreateMap<OrderCreateDto, Order>();
+        CreateMap<OrderUpdateDto, Order>().ReverseMap();
+    }
+}
