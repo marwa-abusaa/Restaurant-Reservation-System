59940da307d449ab8de7000bdc98c657133e1d8c
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-28 15:04:24 +0300

    add Orders model for related DTOs

---
+++ RestaurantReservation.API/Models/Orders/OrderUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantReservation.API.Models.Orders;
+
+public class OrderUpdateDto
+{
+    public decimal TotalAmount { get; set; }
+    public int ReservationId { get; set; }
+    public int EmployeeId { get; set; }
+}
