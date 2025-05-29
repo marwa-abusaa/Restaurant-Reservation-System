98e236fe4b890b31472349cd7abb507237168a94
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-29 15:35:14 +0300

    Add validation rules for orderItems creation and update DTOs

---
+++ RestaurantReservation.API/Validators/OrderItems/OrderItemCreationValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.Models.OrderItems;
+
+namespace RestaurantReservation.API.Validators.OrderItems;
+
+public class OrderItemCreationValidator : AbstractValidator<OrderItemCreateDto>
+{
+    public OrderItemCreationValidator()
+    {
+        RuleFor(x => x.MenuItemId)
+            .NotEmpty().WithMessage("Item ID is required and must be greater than zero.");
+
+        RuleFor(x => x.OrderId)
+            .NotEmpty().WithMessage("Order ID is required must be greater than zero.");
+
+        RuleFor(x => x.Quantity)
+            .GreaterThan(0).WithMessage("Quantity must be greater than zero.");
+    }
+}
