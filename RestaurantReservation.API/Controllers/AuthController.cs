d489e1fd42f6d3c975005190a0808bc931e576dd
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-29 10:45:42 +0300

    Add AuthController to handle user login

---
+++ RestaurantReservation.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.Users;
+using RestaurantReservation.API.Services;
+
+
+namespace RestaurantReservation.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly JwtTokenGenerator _tokenGenerator;
+
+    public AuthController(JwtTokenGenerator tokenGenerator)
+    {
+        _tokenGenerator = tokenGenerator;
+    }
+
+
+    [HttpPost("login")]
+    public IActionResult Login(User requestUser)
+    {
+        var token = _tokenGenerator.GenerateToken(requestUser);
+
+        if (token == null)
+            return Unauthorized("Invalid username or password");
+
+        return Ok(new { token });
+    }
+
+}
