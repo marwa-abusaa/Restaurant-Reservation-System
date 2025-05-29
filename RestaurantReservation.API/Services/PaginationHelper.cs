30e9bae3acff09fd4e28124c75af30481186edbb
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-29 13:31:37 +0300

    Add PaginationHelper class to Implement GetPagedResult for paginated data retrieval

---
+++ RestaurantReservation.API/Services/PaginationHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.API.Models.Paging;
+
+namespace RestaurantReservation.API.Services;
+
+public class PaginationHelper<TEntity>
+{
+    public static async Task<PagedResult<TEntity>> GetPagedResult(IQueryable<TEntity> query, int pageNumber, int pageSize, int MaxPageSize)
+    {
+        if (pageSize > MaxPageSize) pageSize = MaxPageSize;
+
+        var totalCount = await query.CountAsync();
+        var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+
+        if (pageNumber > totalPages && totalPages != 0)
+            pageNumber = totalPages;
+
+        var skip = (pageNumber - 1) * pageSize;
+
+        var items = await query
+            .Skip(skip)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PagedResult<TEntity>
+        {
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalCount = totalCount,
+            TotalPages = totalPages,
+            HasNextPage = pageNumber < totalPages,
+            HasPreviousPage = pageNumber > 1,
+            Items = items
+        };
+    }
+}
