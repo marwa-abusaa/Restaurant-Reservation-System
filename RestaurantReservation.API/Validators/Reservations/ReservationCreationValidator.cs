c78bb5446c70098d9fad6f057310663dd49e3a46
marwa-abusaa <s12028718@stu.najah.edu>
2025-05-29 15:35:49 +0300

    Add validation rules for reservation creation and update DTOs

---
+++ RestaurantReservation.API/Validators/Reservations/ReservationCreationValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.Models.Reservations;
+
+namespace RestaurantReservation.API.Validators.Reservations;
+
+public class ReservationCreationValidator : AbstractValidator<ReservationCreateDto>
+{
+    public ReservationCreationValidator()
+    {
+        RuleFor(x => x.CustomerId)
+            .NotEmpty().WithMessage("Customer ID is required and must be greater than zero.");
+
+        RuleFor(x => x.RestaurantId)
+            .NotEmpty().WithMessage("Restaurant ID is required and must be greater than zero.");
+
+        RuleFor(x => x.TableId)
+            .NotEmpty().WithMessage("Table ID is required and must be greater than zero.");
+
+        RuleFor(x => x.ReservationDate)
+            .GreaterThan(DateTime.Now).WithMessage("Reservation date must be in the future.");
+
+        RuleFor(x => x.PartySize)
+            .GreaterThan(0).WithMessage("Party size must be greater than zero.");
+    }
+}
